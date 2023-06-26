15c54e135827e0b94bc1196a1b1c80720fd05ac2
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-06-26 19:11:34 +0300

    commit

--- ts_start/src/app6.ts
+++ ts_start/src/app6.ts
@@ -6,4 +6,4 @@ if (palindrom === palindrom.split('').reverse().join('')) {
     console.log('заданная строка палиндромом');
 } else {
     console.log('заданная строка не палиндромом');
-}
+}       
