b0e9511db22aa31834c974ea2d07b687cce35bc1
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-07-07 19:23:20 +0300

    commit

--- ts_oop/src/app10.ts
+++ ts_oop/src/app10.ts
@@ -2,3 +2,12 @@
 // price (цена продукта), title (название продукта). Реализуйте метод
 // displayProductInfo(), который выводит массив продуктов, где цена > 20. Создайте
 // экземпляр класса Product и вызовите метод displayProductInfo().
+
+class Product {
+    product = [{ price: 300, title: 'phone' },{ price: 300, title: 'test' },{ price: 300, title: 'test2' }];
+    displayProductInfo() {
+        return this.product.filter(el => el.price > 20);
+    }
+}
+const product = new Product();
+console.log(product.displayProductInfo());
